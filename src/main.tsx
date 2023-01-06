12318666b29076e4dcf34f6f9778d558fb8f0e93
kika <franfonsecav@gmail.com>
2023-01-06 11:24:01 +0100

    tailwind-theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>{/* Your code goes here... */}</React.StrictMode>
+  <React.StrictMode>
+    {<h1 className="text-title bg-bg">hello world</h1>}
+  </React.StrictMode>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      default: ["Inter"],
+    },
+    colors: {
+      bg: "#1C1C27",
+      "bg-light": "#363740",
+      yellow: "#FFB43A",
+      white: "#FFFFFF",
+      "white-dimmed": "rgba(255, 255, 255, 0.4)",
+      "white-dimmed-heavy": "rgba(255, 255, 255, 0.2)",
+      red: "#EF4444",
+      green: "#22C55E",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/line-clamp")],
