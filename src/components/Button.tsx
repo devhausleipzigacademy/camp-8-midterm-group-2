e973708fa74488d245d7854f9729ff26b6e0bebf
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-09 15:47:29 +0100

    updated small size and secondary's button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import "../index.css";
+import { Primary } from "../stores/Button.stories";
 
 interface ButtonProps {
+  primary: boolean;
+  default_height: boolean;
   label: string;
   onClick: (event: React.FormEvent<any>) => void;
-  //primary: boolean; //type
   disabled: boolean; //state
-  //small: boolean; //size
 }
 
 function Button({
-  label, disabled
+  primary, default_height, disabled, label
 }:
 ButtonProps){
   const button = (
     <button
-
       type="button"
       className={
-        `text-center rounded-lg w-full h-12 text-primary text-dark-light ${(disabled)? 'bg-yellow-dimmed':'bg-yellow'}`
+        `text-center rounded-lg w-full text-primary
+        ${(primary)?'bg-yellow text-dark-light':'bg-dark-light text-white'}
+        ${(primary&&disabled)? 'bg-yellow-dimmed':''}
+        ${(default_height)? 'h-[49px]':'h-[38px]'}`
       }
-
       //LINK my props to css Classes -> we will use it in Button.stories.tsx
       // style={{}}
     >
