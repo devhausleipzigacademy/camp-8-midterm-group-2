a592ba7c433f251b0f6d45ecdb4dcff41a59661e
kika <franfonsecav@gmail.com>
2023-01-09 14:23:17 +0100

    styling loginPage

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,12 +1,19 @@
-export function Login() {
+import { Input } from "../components/Input";
+
+export function Login({}) {
   return (
-    <div>
-      <h1 className="text-title">Welcome to Cine-Scape</h1>
-      <p className="text-description">
-        You need to login to be able to make reservations <br /> and add movies
-        to your watchlist.
-      </p>
-      ;
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-3">
+        <h1 className="text-title">Welcome to Cine-Scape</h1>
+        <p className="text-description">
+          You need to login to be able to make reservations <br /> and add
+          movies to your watchlist.
+        </p>
+      </div>
+      <div className="flex flex-col gap-3">
+        <Input type="email" />
+        <Input type="password" />
+      </div>
     </div>
   );
 }
