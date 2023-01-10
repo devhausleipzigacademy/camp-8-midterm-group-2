43400919ec55b904fe833e4e0c22a9a7ab64b08d
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-10 09:42:36 +0100

    Commented out outdated const Template etc

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,46 +1,47 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
 import React from "react";
 import Button from "../components/Button";
-
 
 //LINK our props
 export default {
   component: Button,
   argTypes: {
     //LINK my props to args.
-    primary: { control: 'boolean' },
-    default_height: { control: 'boolean'}
+    primary: { control: "boolean" },
+    default_height: { control: "boolean" },
   },
-} as ComponentMeta<typeof Button> //read type of button and compare it to MetaData that Storybook needs (??)
+};
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
-//what is a template?
+// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+// //what is a template?
 
-export const Primary = Template.bind({}) //what is .bind
-export const Secondary = Template.bind({})
-export const DefaultSize = Template.bind({}) //what is .bind
-export const SmallSize = Template.bind({})
+// export const Primary = Template.bind({}) //what does .bind do
+// export const Secondary = Template.bind({})
+// export const DefaultSize = Template.bind({})
+// export const SmallSize = Template.bind({})
 
 //now specify DEFAULT VALUES OF THE ARGS:
-Primary.args = {
-  primary: true,
-  label: 'default label'
-
+export const Primary = {
+  args: {
+    primary: true,
+    label: "default label",
+  },
 };
 
-Secondary.args = {
-  primary: true,
-  label: 'default label'
+export const Secondary = {
+  args: {
+    primary: true,
+    label: "default label",
+  },
 };
 
-DefaultSize.args = {
-  default_height: true
-}
+export const DefaultSize = {
+  args: {
+    default_height: true,
+  },
+};
 
-SmallSize.args = {
-  default_height: false
-}
-
-
-
-
+export const SmallSize = {
+  args: {
+    default_height: false,
+  },
+};
