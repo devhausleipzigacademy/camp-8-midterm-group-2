8b7f02c7e72dcda984f9fa4848f581c373cdc93d
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-10 16:16:06 +0100

    test navBar in Home()

--- src/layouts/NavBarLayout.tsx
+++ src/layouts/NavBarLayout.tsx
@@ -21,30 +21,30 @@ function NavBarLayout() {
   const navigation = (
     <div className="w-[375px] h-[88px] fixed bottom-0 bg-dark px-[32px] py-[67px]">
       <Outlet />
-      <nav className="flex-col gap-x-[48px]">
+      <nav className="flex-row gap-x-[48px]">
         <NavLink to="/"
         className="w-[24px] h-[24px]">
           <HomeIcon
             key="Home"
-            className={clsx(isActive.home ? "text-white" : "text-white-dimmed")}
+            className={clsx('w-[24px] h-[24px', isActive.home ? "text-white" : "text-white-dimmed")}
           />
         </NavLink>
         <NavLink to="/movies" className="w-[24px] h-[24px]">
           <FilmIcon
             key="Movies"
-            className={clsx(isActive.movies ? "text-white" : "text-white-dimmed")}
+            className={clsx('w-[24px] h-[24px', isActive.movies ? "text-white" : "text-white-dimmed")}
           />
         </NavLink>
         <NavLink to="/#" className="w-[24px] h-[24px]">
           <CircleStackIcon
             key="Wallet"
-            className={clsx(isActive.wallet ? "text-white" : "text-white-dimmed")}
+            className={clsx('w-[24px] h-[24px', isActive.wallet ? "text-white" : "text-white-dimmed")}
           />
         </NavLink>
         <NavLink to="/#" className="w-[24px] h-[24px]">
           <UserIcon
             key="Account"
-            className={clsx(isActive.account ? "text-white" : "text-white-dimmed")}
+            className={clsx('w-[24px] h-[24px', isActive.account ? "text-white" : "text-white-dimmed")}
           />
         </NavLink>
 
