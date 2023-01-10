13a242e66712565e3b9ff04c7b696571e478e3ca
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-10 10:33:07 +0100

    adjusted consts in stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -4,43 +4,36 @@ import Button from "../components/Button";
 export default {
   component: Button,
   argTypes: {
-    //LINK my props to args.
     primary: { control: "boolean" },
-    default_height: { control: "boolean" },
+    SmallSize: { control: "boolean" },
   },
 };
 
-// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
-// //what is a template?
-
-// export const Primary = Template.bind({}) //what does .bind do
-// export const Secondary = Template.bind({})
-// export const DefaultSize = Template.bind({})
-// export const SmallSize = Template.bind({})
-
-//now specify DEFAULT VALUES OF THE ARGS:
 export const Primary = {
   args: {
-    primary: true,
+    type: "primary",
     label: "default label",
   },
 };
 
 export const Secondary = {
   args: {
-    primary: true,
+    type: "secondary",
     label: "default label",
   },
 };
 
 export const DefaultSize = {
   args: {
-    default_height: true,
+    height: "default",
   },
 };
 
 export const SmallSize = {
   args: {
-    default_height: false,
+    height: "small",
   },
 };
+
+////ISSUES
+//how to implement state?
