61ea60604c77e9840604d06b4ff6b45681e7e17c
kika <franfonsecav@gmail.com>
2023-01-10 11:44:37 +0100

    just trying

--- src/components/userInfo.tsx
+++ src/components/userInfo.tsx
@@ -1,5 +1,21 @@
+import React from "react";
+import Avatar from "./public/Avatar.jpg";
 type userInfoProps = {
   userName: string;
+};
+export const userInfoIMG = [
+  (user1 = {
+    userName: "User 1",
+    url: Avatar,
+  }),
+];
+
+const Img = (props: { project: { url: string | undefined } }) => {
+  return (
+    <div>
+      <img src={props.project.url} />
+    </div>
+  );
 };
 
 export function UserInfo({ userName }: userInfoProps) {
@@ -9,7 +25,11 @@ export function UserInfo({ userName }: userInfoProps) {
         <h1 className="text-body">Welcome {userName}!</h1>
         <p className="text-title">Let’s relax and watch a movie!</p>
       </div>
-      <div>{userImg}</div>
+      <React.Fragment>
+        {userInfoIMG.map(() => {
+          return <Img project={user1} key={i} />;
+        })}
+      </React.Fragment>
     </div>
   );
 }
