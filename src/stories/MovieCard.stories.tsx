4af4f32c64dfb66ad4860fcf5ee9cc051366df84
iameru <post@i3o.eu>
2023-01-10 11:10:20 +0100

    improved story

--- src/stories/MovieCard.stories.tsx
+++ src/stories/MovieCard.stories.tsx
@@ -3,9 +3,21 @@ import { Meta, StoryObj } from "@storybook/react";
 import "../index.css";
 import { MovieCard } from "../components/MovieCard";
 
+const imageUrls = [
+  "/src/assets/dev/exampleMovie1.png",
+  "/src/assets/dev/exampleMovie2.png",
+  "/src/assets/dev/exampleMovie3.png",
+  "/src/assets/dev/exampleMovie4.png",
+];
 const meta: Meta<typeof MovieCard> = {
   title: "Movie/MovieCard",
   component: MovieCard,
+  argTypes: {
+    url: {
+      options: imageUrls,
+      control: { type: "radio" },
+    },
+  },
 };
 export default meta;
 
