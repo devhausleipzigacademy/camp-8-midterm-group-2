cd17d13c5459bfa6df57bbb6737f4be166f298e3
kika <franfonsecav@gmail.com>
2023-01-10 14:49:26 +0100

    corrections

--- src/components/userInfo.tsx
+++ src/components/userInfo.tsx
@@ -7,9 +7,9 @@ type userInfoProps = {
 
 export function UserInfo({ userName, userImg }: userInfoProps): JSX.Element {
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex justify-between">
       <div className="flex flex-col gap-3">
-        <h1 className="text-body">Welcome {userName}!</h1>
+        <h1 className="text-body">Welcome {userName}! 👋</h1>
         <p className="text-title">Let’s relax and watch a movie!</p>
       </div>
       <img src={userImg} alt="" className="rounded-full w-10 h-10" />
