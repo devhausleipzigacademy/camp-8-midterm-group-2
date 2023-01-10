d6966c94520df02d26c9d73729cfaa2bafca28ef
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-10 12:33:53 +0100

    Rough structure / react comp for NavBar

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,15 @@
+function Navigation () {
+    const Navigation =
+    (<div className ="w-[375px] h-[88px] fixed bottom-0 bg-dark px-[32px] py-[67px]">
+        <div className="flex-col gap-x-[48px]">
+            <div className="w-[24px] h-[24px]"></div>
+            <div className="w-[24px] h-[24px]"></div>
+            <div className="w-[24px] h-[24px]"></div>
+            <div className="w-[24px] h-[24px]"></div>
+        </div>
+    </div>)
+
+    return Navigation
+}
+
+export default Navigation
