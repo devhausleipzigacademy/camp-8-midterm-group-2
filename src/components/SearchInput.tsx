cfe78b32f77abd658fc108c0a3ce8f5592eff5cb
iameru <post@i3o.eu>
2023-01-11 09:52:13 +0100

    refactoring

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,23 +1,17 @@
-import { useState } from "react";
-import { Form, useSubmit } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import clsx from "clsx";
-import React from "react";
 import "../index.css";
-import { InputProps } from "./Input";
 
 // ! For later Use
 // Link below helps with filling in suggestions to the search input field
 // https://5d9774839a6eff00203f5cbf-przshcqifp.chromatic.com/?path=/story/input-textinput-default-suggestion--default-suggestion
 
 export const SearchInput = () => {
-  const [value, setValue] = React.useState("");
-  const onChange = (event: {
-    target: { value: React.SetStateAction<string> };
-  }) => setValue(event.target.value);
+  const [value, setValue] = useState("");
+  const onChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setValue(event.currentTarget.value);
 
   return (
-    //     id="searchinput"
     <label className="focus-within:border-white-dimmed-heavy flex items-center gap-3 bg-dark-light border-2 rounded-full border-dark-light py-3 px-5 w-full">
       <MagnifyingGlassIcon className="w-6 aspect-square text-white-dimmed" />
       <input
