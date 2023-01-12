edd85498d9d375c138a7ad610b10f849b3a2b3e6
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-12 16:16:49 +0100

    current state (broken)

--- src/routes/MovieDetails.tsx
+++ src/routes/MovieDetails.tsx
@@ -7,9 +7,9 @@ import {
   CurrentMoviePeople,
   Genre,
   MovieDetailsType,
-  props,
 } from "./MovieDetailsTypes";
 
+//what is this again?
 export async function loadMovieDetails(
   loaderObj: any
 ): Promise<CurrentData | undefined> {
@@ -21,6 +21,7 @@ export async function loadMovieDetails(
         `https://api.themoviedb.org/3/movie/${movieId}?api_key=039ceb136bde381a9652fedddb79e1f1`
       )
     ).data;
+
     const people: CurrentMoviePeople = (
       await axios.get(
         `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=039ceb136bde381a9652fedddb79e1f1`
@@ -40,31 +41,37 @@ export async function loadMovieDetails(
 }
 
 function MovieDetails(): JSX.Element {
+
   let { movieId } = useParams(); //what is this?
   const currentData = useLoaderData() as CurrentData;
 
-  function find(role: string) {
+  function find(job: string): string {
     //we are looking through an array
     //currentData.people.cast is an array of objects
+    let Name = ""
     for (const element of currentData.people.cast) {
+      console.log("line 54, element" + element)
       //now we are looking in every object (element <-> current Object)
-      for (const [key, value] of Object.entries(element)) {
-        if (key === role) {
-          return value;
-        } else {
-          return "";
-        }
+      for (const [key] of Object.entries(key)) {
+        if (element.department && (key == job)) {
+          Name = element.department
+          console.log("Line 58 : Name = " + Name)
+          return Name}
+        return Name
       }
     }
+    Name = Name
+    console.log("line 64 , Name" + Name)
+    return Name
   }
 
   const movie_name: string = currentData.details.title;
   const movie_category: Genre[] = currentData.details.genres;
-  const movie_year: number = currentData.details.release_date.getFullYear();
+  // const movie_year: string = (currentData.details.release_date); //getFullYear()
   const movie_score: number = currentData.details.popularity;
   const movie_length: number = currentData.details.runtime;
-  const director: string | "" = find("director");
-  const writer: string | "" = find("writer");
+  const director: string = find("director");
+  const writer: string = find("writer");
   const movie_synopsis = "";
 
   const MovieDetails = (
@@ -78,7 +85,7 @@ function MovieDetails(): JSX.Element {
       <div id="general_details" className="flex flex-col">
         <div className="flex flex-row justify-between">
           <div className="flex flex-row gap-6">
-            <p className="text-white text-description">{movie_year}</p>
+            {/* <p className="text-white text-description">{(movie_year.getFullYear).toString}</p> */}
             <p className="text-white-dimmed text-description">
               {movie_category[0] + "," + movie_category[1]}
             </p>
@@ -125,3 +132,7 @@ function MovieDetails(): JSX.Element {
 }
 
 export default MovieDetails;
+
+
+//http://localhost:5173/315162 EXAMPLE URL TO TEST
+//https://api.themoviedb.org/3/movie/315162/credits?api_key=039ceb136bde381a9652fedddb79e1f1
