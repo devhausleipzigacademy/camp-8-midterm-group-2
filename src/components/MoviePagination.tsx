aaa4cda6a522078ff25505cdd9bce718c7f47063
iameru <post@i3o.eu>
2023-01-13 10:25:00 +0100

    added movie pagination component

---
+++ src/components/MoviePagination.tsx
@@ -0,0 +1,3 @@
+export function MoviePagination() {
+  return <>MOVIEPAGINATION</>;
+}
--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,7 +1,13 @@
 import { useLoaderData } from "react-router-dom";
+import { MoviePagination } from "../components/MoviePagination";
 
 export function Movies(): JSX.Element {
   const upcomingMovies = useLoaderData() as Movie[];
 
-  return <section className="flex-1"></section>;
+  return (
+    <section className="flex-1">
+      {/* // Here to develop as I cant handle storybook with a loader function */}
+      <MoviePagination />
+    </section>
+  );
 }
