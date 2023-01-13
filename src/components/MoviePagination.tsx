b366db556acccc15e25de229de5eb6236451f2a5
iameru <post@i3o.eu>
2023-01-13 11:29:15 +0100

    styled the component

--- src/components/MoviePagination.tsx
+++ src/components/MoviePagination.tsx
@@ -2,6 +2,7 @@ import { Movie } from "../types/api";
 import { useState } from "react";
 import { MovieCard } from "./MovieCard";
 import { Link } from "react-router-dom";
+import clsx from "clsx";
 
 type MoviePaginationProps = {
   upcomingMovies: Movie[];
@@ -13,16 +14,20 @@ export function MoviePagination({
 }: MoviePaginationProps) {
   const [movies, setMovies] = useState(upcomingMovies.slice(0, 4));
 
-  console.log(upcomingMovies.length);
   const pages = Math.floor(upcomingMovies.length / itemsPerPage);
   const pageArray = Array.from({ length: pages }, (_, i) => i + 1);
 
+  const [currentPage, setCurrentPage] = useState(pageArray[0]);
+
   return (
-    <>
-      <div className="text-white text-3xl">
+    <div className="flex flex-col gap-7 w-full h-full">
+      <div className="text-white flex-1 text-3xl grid grid-cols-2 gap-5">
         {movies.map((movie) => {
           return (
-            <Link to={movie.id.toString()}>
+            <Link
+              to={movie.id.toString()}
+              className="flex justify-center items-center w-full h-full"
+            >
               <MovieCard
                 poster_path={movie.poster_path}
                 title={movie.title}
@@ -39,10 +44,16 @@ export function MoviePagination({
 
           return (
             <button
-              className="bg-yellow p-3 rounded"
+              className={clsx(
+                "py-2 px-4 rounded-sm text-dark-light ",
+                currentPage === pageNr
+                  ? "bg-yellow"
+                  : "bg-white-dimmed hover:bg-yellow-dimmed"
+              )}
               key={pageNr}
-              onClick={(event) => {
+              onClick={() => {
                 setMovies(upcomingMovies.slice(startPagination, endPagination));
+                setCurrentPage(pageNr);
               }}
             >
               {pageNr}
@@ -50,6 +61,6 @@ export function MoviePagination({
           );
         })}
       </div>
-    </>
+    </div>
   );
 }
