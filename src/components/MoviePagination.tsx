5c9381cb7317f43a80940aace537c9d661c10a9f
iameru <post@i3o.eu>
2023-01-13 10:36:51 +0100

    Proof of concept pagination

--- src/components/MoviePagination.tsx
+++ src/components/MoviePagination.tsx
@@ -1,3 +1,33 @@
-export function MoviePagination() {
-  return <>MOVIEPAGINATION</>;
+import { Movie } from "../types/api";
+import { useState } from "react";
+
+type MoviePaginationProps = {
+  upcomingMovies: Movie[];
+};
+export function MoviePagination({ upcomingMovies }: MoviePaginationProps) {
+  const [movies, setMovies] = useState(upcomingMovies.slice(0, 4));
+  return (
+    <>
+      <div className="text-white text-3xl">
+        {movies.map((movie) => {
+          return <div>{movie.title}</div>;
+        })}
+      </div>
+      <div className="flex justify-around">
+        <button className="bg-yellow p-3" id="1">
+          1
+        </button>
+        <button
+          className="bg-yellow p-3"
+          id="2"
+          onClick={(event) => {
+            event.currentTarget.id;
+            setMovies(upcomingMovies.slice(4, 8));
+          }}
+        >
+          2
+        </button>
+      </div>
+    </>
+  );
 }
