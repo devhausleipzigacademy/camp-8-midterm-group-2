1d849e89a669424e0a69fe3cbe892161afb25aa1
iameru <post@i3o.eu>
2023-01-16 16:07:52 +0100

    added types for dates

--- src/routes/SelectTime.tsx
+++ src/routes/SelectTime.tsx
@@ -1,5 +1,16 @@
-export function selectTimeLoader() {
-  return null;
+import { DatesType } from "../types/times";
+
+export function selectTimeLoader(): DatesType {
+  return {
+    "0000T0000": {
+      "00000T1234": { bookable: true },
+      "00000T1235": { bookable: true },
+    },
+    "0001T0000": {
+      "00000T1234": { bookable: true },
+      "00000T1235": { bookable: false },
+    },
+  };
 }
 
 export function SelectTime(): JSX.Element {
