ea4e7064b0ba54fcc0ffe2361ffe63074db1573f
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-16 12:13:32 +0100

    general: bg-color and screen-size

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,8 +2,8 @@ import NavBarLayout from "../layouts/NavBarLayout";
 
 export function Home(): JSX.Element {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="bg-dark h-[667px] w-[375px] fixed m-0 top-0">
+      <h1>Home Placeholder</h1>
     </div>
   );
 }
