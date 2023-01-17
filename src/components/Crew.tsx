a55397e80a5e3c2324a21ecb25072cd974f74f71
kika <franfonsecav@gmail.com>
2023-01-17 11:26:23 +0100

    trying with crew

--- src/components/Crew.tsx
+++ src/components/Crew.tsx
@@ -1,45 +1,96 @@
 import React from "react";
 
-type props = {
-  image: string;
-  name: string;
-  character: string;
+import axios from "axios";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom"; //what for Outlet
+import Button from "../components/Button";
+import { Credits, MovieDetail } from "../types/api";
+import { useState } from "react";
+import { posterUrl } from "../utils/movies";
+
+type CurrentData = {
+  details: MovieDetail;
+  director: string;
+  writer: string;
+  poster_path: string;
 };
 
-const crew = [
-  {
-    image:
-      "https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg",
-    name: "Sebi",
-    character: "Frontend-Developer",
-  },
-  {
-    image:
-      "https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg",
-    name: "Kika",
-    character: "Frontend-Developer",
-  },
-  {
-    image:
-      "https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg",
-    name: "Thomas",
-    character: "Frontend-Developer",
-  },
-];
+export async function loadMovieDetails(loaderObj: any) {
+  const movieId = loaderObj.params.movieId;
 
-export function Crew() {
-  return (
-    <div>
-      {crew.map((actor) => (
-        <div className="flex items-center gap-5">
-          <img src={actor.image} alt="" className="w-16 h-16" />
-          <div className="flex flex-col gap-1">
-            <div className="text-title"> {actor.name} </div>
-            <div className="text-description"> {actor.character}</div>
-          </div>
-        </div>
-      ))}
-      ;
+  try {
+    //returns promise; .data takes data
+    const details: MovieDetail = (
+      await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=039ceb136bde381a9652fedddb79e1f1`
+      )
+    ).data;
+
+    //returns promise; .data takes data
+    const people: Credits = (
+      await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=039ceb136bde381a9652fedddb79e1f1`
+      )
+    ).data;
+
+    const director_name = people.crew.find(
+      (member) => member.job === "Director"
+    )?.name; //returned member, if 39 === true
+
+    const writer_name = people.crew.find(
+      (member) => member.job === "Writer"
+    )?.name;
+
+    const currentData = {
+      details: details,
+      director: director_name,
+      writer: writer_name,
+    };
+
+    return currentData;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+function MovieDetails(): JSX.Element {
+  let { movieId } = useParams();
+
+  const navigate = useNavigate();
+
+  const [state, setState] = useState("start");
+
+  const currentData = useLoaderData() as CurrentData;
+
+  const movie_score: number = currentData.details.popularity;
+  const movie_length: number | null = currentData.details.runtime;
+  const director: string = currentData.director;
+  const poster_path: string = posterUrl + currentData.details.poster_path;
+
+  //content wrapper contains: img, details-wrapper, button
+  //page_wrapper will contain Navigation and Content Wrapper
+  //pt-[75px] goes away when Nav gets integrated
+  //bg-dark h-[667px] w-[375px] goes in Home Componente oder in tailwind-theme fÃ¼r #root
+  //mb-[50px] for details_wrapper SHPOULD NO BE THERE
+  //absolute / fixed positioning does NOT work for buttom (??!!??)
+
+  const MovieDetails = (
+    <div
+      id="image_wrapper"
+      className="h-[211px] pt-0 mb-6 p-0 w-full overflow-hidden rounded-lg"
+    >
+      <img
+        alt=""
+        className="object-cover relative top-[-40%]"
+        src={poster_path}
+      ></img>
     </div>
   );
+
+  return MovieDetails;
 }
+
+export default MovieDetails;
+
+//http://localhost:5173/315162 EXAMPLE URL TO TEST
+//https://api.themoviedb.org/3/movie/315162/credits?api_key=039ceb136bde381a9652fedddb79e1f1
