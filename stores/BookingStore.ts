e087ff4ebfecc986fabc2ce5957497f539fb99ab
iameru <post@i3o.eu>
2023-01-18 16:19:30 +0100

    broken zustand

--- stores/BookingStore.ts
+++ stores/BookingStore.ts
@@ -1,3 +1,6 @@
+import create from "zustand/react"
+import { persist } from 'zustand/middleware'
+
 type CategoryType = 'front' | 'middle' | 'back'
 
 type row = 'A' | 'B' | 'C' | 'D' | 'E' | 'F'
@@ -10,3 +13,28 @@ type Seat = {
   seat: seat
   available: boolean
 }
+type Seats = {
+  [key in seat]: 
+}
+
+type SeatStore = {
+  seats: Seats
+  setSeat: (seat: Seat) => void
+  clear: () => void
+}
+
+
+const initialState = {
+  seats: {}
+}
+
+
+export const useSeatStore = create<SeatStore>()(
+
+  persist((set) => ({
+    ...initialState,
+    setSeat: (seat) => set({ seat }),
+    clear: () => set({ ...initialState })
+
+  }), { name: 'seat-store' })
+)
