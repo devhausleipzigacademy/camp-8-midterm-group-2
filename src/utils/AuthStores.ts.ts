193ca13abc327abc667d154ab5ae10497210816a
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-18 14:25:30 +0100

    done!

--- src/utils/AuthStores.ts.ts
+++ src/utils/AuthStores.ts.ts
@@ -9,12 +9,34 @@ type User = {
 
 //Create a type for your AuthStore that can hold a user and a token, as well as updaters
 type AuthStore = {
-    email: string;
     token: string;
     user: User
-    setEmail: (inputEmail: string) => void;
+    setUser: (user: User) => void; //setUser receives an updated user later
     setToken: (tokenResponse: string) => void;
     clear: () => void;
   };
 
+//implement an initialState
+const initialState = {
+    token: "",
+    user: {
+        username: "",
+        avatarUrl: "",
+    },
+}
+
 //implement the store (state values and setters)
+export const useAuthStore = create<AuthStore>()(
+    persist(
+    //what does set do exactly
+      (set) => ({
+        ...initialState,
+        setUser: (user) => set({ user }),
+        setToken: (token) => set({ token }),
+        clear: () => set({ ...initialState }),
+      }),
+      {
+        name: "user-auth",
+      }
+    )
+  );
