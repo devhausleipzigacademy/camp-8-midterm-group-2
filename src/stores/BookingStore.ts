7e875a7ba99a1ece097a6bd3d003e7bad33387a9
iameru <post@i3o.eu>
2023-01-19 14:48:14 +0100

    just storing an array of strings now

--- src/stores/BookingStore.ts
+++ src/stores/BookingStore.ts
@@ -1,63 +1,20 @@
 import create from "zustand"
 import { persist } from 'zustand/middleware'
 
-type CategoryType = 'front' | 'middle' | 'back'
-
-type row = 'A' | 'B' | 'C' | 'D' | 'E' | 'F'
-type col = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8'
-type seat = `${row}-${col}`
-
-
-type SeatType = {
-  category: CategoryType
-  id: seat
-  available: boolean
-}
-// const theSeatObject: SeatType = {
-//   category: "back",
-//   id: "A-7",
-//   available: true
-// }
-
-type Seats = {
-  // sadly didnt work :
-  // [key in seat]: Seat
-  // [key: seat]: Seat
-  [key: string]: SeatType
-}
-
 type SeatStore = {
-  seatsChosen: Seats | {}
-  addSeatChosen: (seat: SeatType) => void
-  removeSeatChosen: (seat: SeatType) => void
+  seatsChosen: string[] | []
+  setSeatsChosen: (seats: string[]) => void
   clear: () => void
 }
 
-
-
-
-
 export const useSeatStore = create<SeatStore>()(
 
-  persist((set, get) => ({
+  persist((set) => ({
 
-    seatsChosen: {},
-    addSeatChosen: (seat) => {
-
-      const seatsChosen = get().seatsChosen
-      seatsChosen[seat.id] = seat
-      set({ seatsChosen: seatsChosen })
-    },
-
-    removeSeatChosen: (seat) => {
-
-      const seatsChosen = get().seatsChosen
-      delete seatsChosen[seat.id]
-      set({ seatsChosen: seatsChosen })
-    },
-
+    seatsChosen: [],
+    setSeatsChosen: (seats) => set({ seatsChosen: seats }),
     clear: () => {
-      set({ seatsChosen: {} })
+      set({ seatsChosen: [] })
     }
 
   }), { name: 'seat-store' })
