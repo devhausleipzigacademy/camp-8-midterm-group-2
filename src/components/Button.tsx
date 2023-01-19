f9c9c760deb09eae975a0410d1cce3f9484af0d6
Franz Wollang <franzwollang@gmail.com>
2023-01-19 16:48:39 +0100

    fix typos and add default value

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,34 +1,38 @@
 import clsx from "clsx";
 import React from "react";
 
-
 interface ButtonProps {
   type: "primary" | "secondary";
-  height: "default" | "small"
+  height: "default" | "small";
   label: string;
   onClick: (event: React.FormEvent<any>) => void;
-  disabled?: true; //state
+  disabled?: boolean;
 }
 
 function Button({
-  variant, height, label, disabled, ...props}:
-ButtonProps){
+  type,
+  height,
+  label,
+  disabled = false,
+  ...props
+}: ButtonProps) {
   return (
     <button
       type="button"
-      className=
-      {clsx(
-        'text-center rounded-lg w-full text-primary',
-        (type === "primary" && !disabled)? 'bg-yellow text-dark-light' : '',
-        (type === "primary" && disabled)? 'bg-yellow-dimmed text-dark-light' : '',
-        (height === "default")? 'py-[16px]':'pt-[12px] pb-[11px]',
-        (variant === "secondary")? 'bg-dark-light text-white':'')}
-        {...props}
+      className={clsx(
+        "text-center rounded-lg w-full text-primary",
+        type === "primary" && !disabled ? "bg-yellow text-dark-light" : "",
+        type === "primary" && disabled
+          ? "bg-yellow-dimmed text-dark-light"
+          : "",
+        height === "default" ? "py-[16px]" : "pt-[12px] pb-[11px]",
+        type === "secondary" ? "bg-dark-light text-white" : ""
+      )}
+      {...props}
     >
       {label}
     </button>
   );
-
-};
+}
 
 export default Button;
