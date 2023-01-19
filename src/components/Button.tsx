a81b22afad83c23c48af810d1d7bcd025761125b
Franz Wollang <franzwollang@gmail.com>
2023-01-19 16:37:19 +0100

    Remove changes unrelated to PR

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,35 +1,35 @@
 import clsx from "clsx";
-import React from "react";
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
-
-interface ButtonProps {
-
-  type: "primary" | "secondary";
-  height: "default" | "small"
+export interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  variant: "primary" | "secondary";
+  height: "default" | "small";
   label: string;
-  onClick: (event: React.FormEvent<any>) => void;
-  disabled?: true; //state
+  disabled?: true;
 }
 
-function Button({
-  type, height, label, disabled
-}:
-ButtonProps){
-  const button = (
+function Button({ variant, height, label, disabled, ...props }: ButtonProps) {
+  return (
     <button
       type="button"
-      className=
-      {clsx(
-        'text-center rounded-lg w-full text-primary',
-        (type === "primary" && disabled)? 'bg-yellow text-dark-light' : '',
-        (type ==="primary" && !disabled)? 'bg-yellow-dimmed text-dark-light' : '',
-        (height === "default")? 'py-[16px]':'pt-[12px] pb-[11px]',
-        (type === "secondary")? 'bg-dark-light text-white':'')}
+      className={clsx(
+        "text-center rounded-lg w-full text-primary",
+        variant === "primary" && !disabled ? "bg-yellow text-dark-light" : "",
+        variant === "primary" && disabled
+          ? "bg-yellow-dimmed text-dark-light"
+          : "",
+        height === "default" ? "py-[16px]" : "pt-[12px] pb-[11px]",
+        variant === "secondary" ? "bg-dark-light text-white" : ""
+      )}
+      {...props}
     >
       {label}
     </button>
   );
-  return button;
-};
+}
 
 export default Button;
