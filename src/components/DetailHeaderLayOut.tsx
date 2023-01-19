390dee1628ff5354ff9c71b8a75d9ab3772ebaab
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-01-19 15:46:05 +0100

    DetailsHeader styled

--- src/components/DetailHeaderLayOut.tsx
+++ src/components/DetailHeaderLayOut.tsx
@@ -8,15 +8,13 @@ type props = {
 export function DetailHeader({ title }: props): JSX.Element {
   const navigate = useNavigate();
   return (
-    <header
-      className="w-full absolute top-8 mx-4 flex"
-    >
-      <div onClick={() => navigate(-1)}>
-        <ChevronLeftIcon className="h-6 fill-white" />
+    <header className="pt-8 pl-4 pr-4 flex items-center justify-between">
+      <div className="w-6 h-6" onClick={() => navigate(-1)}>
+        <ChevronLeftIcon className="self-center ml-1 h-6 fill-white" />
       </div>
-      <h1 className="text-title absolute inset-y-1/2">{title}</h1>
-      <div />
-      <div className="flex-1"/>
+      <h1 className="text-title">{title}</h1>
+      <div className="w-6 h-6"/>
+
     </header>
   );
 }
