ed0b26d3a958f197933b84d5a550bd560fe0ad9d
iameru <post@i3o.eu>
2023-01-20 10:04:06 +0100

    fixed typing

--- src/stores/BookingStore.ts
+++ src/stores/BookingStore.ts
@@ -2,7 +2,7 @@ import create from "zustand"
 import { persist } from 'zustand/middleware'
 
 type SeatStore = {
-  seatsChosen: string[] | []
+  seatsChosen: string[]
   setSeatsChosen: (seats: string[]) => void
   clear: () => void
 }
