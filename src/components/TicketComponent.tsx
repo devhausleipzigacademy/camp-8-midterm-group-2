dea5a10e570e11408600480111189b6c923ab436
Thomas <sappeiduda@gmail.com>
2023-01-20 16:09:28 +0100

    some functionality added, some styling missing

--- src/components/TicketComponent.tsx
+++ src/components/TicketComponent.tsx
@@ -1,39 +1,60 @@
+import React from "react";
 import { FormEvent } from "react";
 import Barcode from "react-barcode";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import Button from "./Button";
 
-export function TicketComponent(): JSX.Element {
+export type TicketProps = {
+  title: string;
+  date: string;
+  time: string;
+  price: string;
+  seats: Array<string>;
+};
+
+export function TicketComponent({
+  title,
+  date,
+  time,
+  price,
+  seats,
+}: TicketProps): JSX.Element {
+  let navigate = useNavigate();
+
   return (
     <div className="flex flex-col gap-6">
-      <div className="divide-y divide-dashed divide-white   bg-dark-light h-full rounded-xl">
+      <div className="divide-y divide-dashed divide-white bg-dark-light h-full rounded-xl">
         <div className="">
           <img
             src="src/assets/dev/exampleMovie1.png"
-            alt=""
+            alt="Movie picture"
             className="h-40 w-full rounded-t-xl"
           />
-          <div className="px-6">
-            <h1 className="text-title text-2xl pt-2">Violent Night</h1>
-            <div className="flex justify-between pt-4">
-              <div className="flex flex-col gap-1">
-                <p className="text-description">Date</p>
-                <p className="text-title">08 Jan</p>
-              </div>
-              <div className="flex flex-col gap-1">
-                <p className="text-description">Date</p>
-                <p className="text-title">08 Jan</p>
-              </div>
-              <div className="flex flex-col gap-1">
-                <p className="text-description">Date</p>
-                <p className="text-title">08 Jan</p>
-              </div>
+          <div className="px-6 grid grid-cols-3 grid-rows-5">
+            <h1 className="text-title text-2xl col-span-3 ">{title}</h1>
+
+            <div className="flex flex-col gap-1">
+              <p className="text-description">Date</p>
+              <p className="text-title">{date}</p>
             </div>
-            <div className="flex flex-col gap-1 pb-32">
+            <div className="flex flex-col gap-1">
+              <p className="text-description">Time</p>
+              <p className="text-title">{time}</p>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="text-description">Price</p>
+              <p className="text-title">{price}</p>
+            </div>
+
+            <div className="flex flex-col gap-1 col-span-full ">
               <p className="text-description pt-4">Seats</p>
-              <p className="text-title">C-3, C-5, A-4</p>
+              <p className="text-title">{seats}</p>
             </div>
           </div>
+        </div>
+        <div className="h-0 w-full flex justify-between">
+          <div className="h-12 w-6 rounded-r-full bg-dark -translate-y-6"></div>
+          <div className="h-12 w-6 rounded-l-full bg-dark -translate-y-6"></div>
         </div>
         <div className="flex justify-center">
           <Barcode
@@ -51,9 +72,7 @@ export function TicketComponent(): JSX.Element {
         type={"primary"}
         height={"default"}
         label={"Back To Home"}
-        onClick={function (event: FormEvent<any>) {
-          return <Navigate to="/" replace />;
-        }}
+        onClick={() => navigate("/")}
         disabled={false}
       ></Button>
     </div>
--- src/routes/Ticket.tsx
+++ src/routes/Ticket.tsx
@@ -1,9 +1,24 @@
-import { TicketComponent } from "../components/TicketComponent";
+import { TicketComponent, TicketProps } from "../components/TicketComponent";
+
+const ticketInfo: TicketProps = {
+  title: "",
+  date: "",
+  time: "",
+  price: "",
+  seats: [],
+};
 
 export function Ticket(): JSX.Element {
   return (
     <div className="bg-dark h-screen  pt-8 px-5">
-      <TicketComponent />;
+      <TicketComponent
+        title={"Violent Night"}
+        date={"08. Jan"}
+        time={"14:30"}
+        price={"$42.85"}
+        seats={["C-3", "C-5", "A-4"]}
+      />
+      ;
     </div>
   );
 }
