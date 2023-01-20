e99301324c32ad60bf1cf5aab065188f9dacf984
Franz Wollang <franzwollang@gmail.com>
2023-01-20 12:12:35 +0100

    remove placeholder

--- src/layouts/MoviesLayout.tsx
+++ src/layouts/MoviesLayout.tsx
@@ -4,7 +4,6 @@ import { DetailHeader } from "../components/DetailHeaderLayout";
 export function MovieLayout() {
   return (
     <div className="bg-dark px-5 pt-8 pb-6 h-screen text-white">
-      <header>chevronBack</header>
       <Outlet />
     </div>
   );
