66f829c323e215c77db700ae18cdf90ff6462b47
acz1992 <116277029+acz1992@users.noreply.github.com>
2023-01-23 11:21:50 +0100

    trying to put restrictions on button and transition

--- src/components/CheckoutPanel.tsx
+++ src/components/CheckoutPanel.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
+import { SeatSelectButton } from "./SeatSelectButton";
+import { Transition } from "@headlessui/react";
 
-type CheckoutPanelProps = {
+export type Seats = {
+  type: string;
+};
+
+export type CheckoutPanelProps = {
+  seats: Seats[];
   totalAmount: number;
+  click: () => void;
 };
 
 export function CheckoutPanel() {
@@ -12,62 +20,74 @@ export function CheckoutPanel() {
   /* const totalAmount =  */
 
   return (
-    <div className="bg-dark-light rounded-3xl pt-7">
-      {/* wrapper within background */}
-      <div className="px-5 pb-6 ">
-        {/* wrapper for quantity and category */}
-        <div className="">
-          <div className="flex justify-between py-2">
-            <div className="flex  gap-x-6">
-              <p className="text-description text-white-dimmed">1x</p>
-              <p className="text-description text-white">Seat - Front</p>
+    <div>
+      <Transition
+        show={seats.length > 0}
+        enter="transition-opacity duration-75"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-150"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      ></Transition>
+
+      <div className="bg-dark-light rounded-3xl pt-7">
+        {/* wrapper within background */}
+        <div className="px-5 pb-6 ">
+          {/* wrapper for quantity and category */}
+          <div className="">
+            <div className="flex justify-between py-2">
+              <div className="flex  gap-x-6">
+                <p className="text-description text-white-dimmed">1x</p>
+                <p className="text-description text-white">Seat - Front</p>
+              </div>
+              <div className="flex gap-x-1">
+                <p className="text-description text-white">$12.95</p>
+                <p className="text-description text-white-dimmed">/ each</p>
+              </div>
             </div>
-            <div className="flex gap-x-1">
-              <p className="text-description text-white">$12.95</p>
-              <p className="text-description text-white-dimmed">/ each</p>
+
+            <div className="flex justify-between py-2">
+              <div className="flex  gap-x-6">
+                <p className="text-description text-white-dimmed">2x</p>
+                <p className="text-description text-white">Seat - Middle</p>
+              </div>
+              <div className="flex gap-x-1">
+                <p className="text-description text-white">$14.95</p>
+                <p className="text-description text-white-dimmed">/ each</p>
+              </div>
+            </div>
+
+            <div className="flex justify-between py-2">
+              <div className="flex  gap-x-6">
+                <p className="text-description text-white-dimmed">2x</p>
+                <p className="text-description text-white">Seat - Back</p>
+              </div>
+              <div className="flex gap-x-1">
+                <p className="text-description text-white">$16.95</p>
+                <p className="text-description text-white-dimmed">/ each</p>
+              </div>
             </div>
           </div>
 
-          <div className="flex justify-between py-2">
-            <div className="flex  gap-x-6">
-              <p className="text-description text-white-dimmed">2x</p>
-              <p className="text-description text-white">Seat - Middle</p>
+          <div className="pt-2">
+            <div className="bg-white-dimmed-heavy h-[1px]"></div>
+            {/* wrapper for whole of the bottom half */}
+            <div className="flex mt-8 space-x-12">
+              {/* wrapper containing the total */}
+              <div className="flex-col space-y-[3px]">
+                <p className="text-description">Total Price</p>
+                <p className="font-bold text-[20px] text-white">$43.55</p>
+              </div>
+              <Button
+                variant={seats.length > 0 ? "primary" : "secondary"}
+                height="default"
+                label={"Book Tickets"}
+                onClick={() => {
+                  () => navigate(`ticket`);
+                }}
+              />
             </div>
-            <div className="flex gap-x-1">
-              <p className="text-description text-white">$14.95</p>
-              <p className="text-description text-white-dimmed">/ each</p>
-            </div>
-          </div>
-
-          <div className="flex justify-between py-2">
-            <div className="flex  gap-x-6">
-              <p className="text-description text-white-dimmed">2x</p>
-              <p className="text-description text-white">Seat - Back</p>
-            </div>
-            <div className="flex gap-x-1">
-              <p className="text-description text-white">$16.95</p>
-              <p className="text-description text-white-dimmed">/ each</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="pt-2">
-          <div className="bg-white-dimmed-heavy h-[1px]"></div>
-          {/* wrapper for whole of the bottom half */}
-          <div className="flex mt-8 space-x-12">
-            {/* wrapper containing the total */}
-            <div className="flex-col space-y-[3px]">
-              <p className="text-description">Total Price</p>
-              <p className="font-bold text-[20px] text-white">$43.55</p>
-            </div>
-            <Button
-              variant="primary"
-              height="default"
-              label={"Book Tickets"}
-              onClick={() => {
-                () => navigate(`ticket`);
-              }}
-            />
           </div>
         </div>
       </div>
