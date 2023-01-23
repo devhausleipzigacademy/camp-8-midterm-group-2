230e777c1d0a3bed03fb7fcc6c5440b9ccf2866a
kika <franfonsecav@gmail.com>
2023-01-23 10:16:29 +0100

    last changes

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,30 +1,54 @@
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import Button from "../components/Button";
 import { Input } from "../components/Input";
+
+type User = {
+  email: string;
+  password: string;
+};
+
+const userInput: User = {
+  email: "",
+  password: "",
+};
 
 function testAPI(email: string, password: string) {
   const exampleUser = { email: "kike@hotmail.com", password: "1234" };
   if (email === exampleUser.email)
     if (password === exampleUser.password) {
-      return true;
+      console.log(email);
+      return <Navigate to="/" replace />;
     }
   return false;
 }
 
-export function Login() {
+export function Login(): JSX.Element {
   const [emailState, setEmailState] = useState("");
   const [passwordState, setPasswordState] = useState("");
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    testAPI(emailState, passwordState);
+  };
 
   return (
-    <form className="flex flex-col justify-between gap-8 bg-dark h-screen px-5 py-8">
+    <form
+      className="flex flex-col justify-between gap-8 bg-dark h-screen w-screen px-5 py-8"
+      onSubmit={handleSubmit}
+    >
       <div className="flex flex-col gap-3">
         <h1 className="text-title">Welcome to Cine-Scape</h1>
         <p className="text-description">
           You need to login to be able to make reservations <br /> and add
           movies to your watchlist.
         </p>
-        <Input type="email" onChange={() => setEmailState(emailState)} />
         <Input
+          className="max-width"
+          type="email"
+          onChange={() => setEmailState(emailState)}
+        />
+        <Input
+          className="max-width"
           type="password"
           onChange={() => setPasswordState(passwordState)}
         />
