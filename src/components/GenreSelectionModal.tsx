f65add5b34a9fcdd07d69fb6d49727619e6121f8
iameru <post@i3o.eu>
2023-01-23 12:10:45 +0100

    added selected state and styling to indicate

--- src/components/GenreSelectionModal.tsx
+++ src/components/GenreSelectionModal.tsx
@@ -3,12 +3,15 @@ import { genres } from "../utils/genres";
 import { capitalizeString } from "../utils/capitalize";
 import { Dialog } from "@headlessui/react";
 import { useState } from "react";
+import clsx from "clsx";
 
 type props = {
   open: boolean;
 };
+type selectedStateType = [] | string[];
 export function GenreSelectionModal({ open = false }: props) {
   const [isOpen, setIsOpen] = useState(open);
+  const [selected, setSelected] = useState<selectedStateType>([]);
   return (
     <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
       <Dialog.Panel>
@@ -17,7 +20,20 @@ export function GenreSelectionModal({ open = false }: props) {
             return (
               <a
                 key={genre}
-                className="flex flex-col gap-2 items-center hover:cursor-pointer hover:first:bg-white"
+                className={clsx(
+                  selected.includes(genre) && "[&>button]:bg-white-dimmed",
+                  "flex flex-col gap-2 items-center hover:cursor-pointer hover:first:bg-white"
+                )}
+                onClick={() => {
+                  const indexSelected = selected.indexOf(genre);
+                  if (indexSelected > -1) {
+                    const selCopy = selected.slice();
+                    selCopy.splice(indexSelected, 1);
+                    setSelected([...selCopy]);
+                  } else {
+                    setSelected([...selected, genre]);
+                  }
+                }}
               >
                 <GenreButton genre={genre} />
                 <p className="font-bold text-xs text-white-dimmed">
