490236cfd0f1d646d10a6a9252c561ca695af32d
acz1992 <116277029+acz1992@users.noreply.github.com>
2023-01-31 15:08:47 +0100

    latest changes

--- src/components/CheckoutPanel.tsx
+++ src/components/CheckoutPanel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 import { SeatSelectButton } from "./SeatSelectButton";
@@ -6,18 +6,40 @@ import { Transition } from "@headlessui/react";
 import { useSeatStore } from "../stores/BookingStore";
 
 export type CheckoutPanelProps = {
-  seatsChosen: [];
-  /*   totalAmount: number;
-  click: () => void; */
+  // Pass in prop of Seats Chosen from BookingStore
+  seatsChosen: string[];
+  click: () => void;
 };
 
-export function CheckoutPanel({ seatsChosen }: CheckoutPanelProps) {
+const pricesCategory: Record<string, number> = {
+  Front: 12.95,
+  Middle: 14.95,
+  Back: 16.95,
+};
+
+export function CheckoutPanel({ seatsChosen, click }: CheckoutPanelProps) {
+  // For Button at the bottom to navigate to ticket
   const navigate = useNavigate();
-  /*   const { seatsChosen } = useSeatStore(); */
-
-  console.log(seatsChosen);
-
-  
+  // Creates state of quantities, sets initial values for each section of seats
+  const [quantities, setQuantities] = useState({
+    front: 0,
+    middle: 0,
+    back: 0,
+  });
+  // Call a function when state (here setQuantities) changes
+  useEffect(() => {
+    const frontRowSeats = seatsChosen.filter(
+      (seat) => seat.startsWith("A") || seat.startsWith("B")
+    );
+    const backRowSeats = seatsChosen.filter(
+      (seat) => seat.startsWith("E") || seat.startsWith("F")
+    );
+    setQuantities({
+      front: frontRowSeats.length,
+      back: backRowSeats.length,
+      middle: seatsChosen.length - frontRowSeats.length - backRowSeats.length,
+    });
+  }, [seatsChosen]);
 
   /* const totalAmount =  */
 
@@ -28,38 +50,58 @@ export function CheckoutPanel({ seatsChosen }: CheckoutPanelProps) {
         <div className="px-5 pb-6 ">
           {/* wrapper for quantity and category */}
           <div className="">
-            <div className="flex justify-between py-2">
-              <div className="flex  gap-x-6">
-                <p className="text-description text-white-dimmed">1x</p>
-                <p className="text-description text-white">Seat - Front</p>
+            {/* when front seats quantities greater than 0, retruns div */}
+            {quantities.front > 0 && (
+              <div className="flex justify-between py-2">
+                <div className="flex  gap-x-6">
+                  <p className="text-description text-white-dimmed">
+                    {quantities.front}x
+                  </p>
+                  <p className="text-description text-white">Seat - Front</p>
+                </div>
+                <div className="flex gap-x-1">
+                  {/* interacting with state and price Category Object */}
+                  <p className="text-description text-white">
+                    ${quantities.front * pricesCategory.front}
+                  </p>
+                  <p className="text-description text-white-dimmed">/ each</p>
+                </div>
               </div>
-              <div className="flex gap-x-1">
-                <p className="text-description text-white">$12.95</p>
-                <p className="text-description text-white-dimmed">/ each</p>
-              </div>
-            </div>
+            )}
 
-            <div className="flex justify-between py-2">
-              <div className="flex  gap-x-6">
-                <p className="text-description text-white-dimmed">2x</p>
-                <p className="text-description text-white">Seat - Middle</p>
+            {quantities.middle > 0 && (
+              <div className="flex justify-between py-2">
+                <div className="flex  gap-x-6">
+                  <p className="text-description text-white-dimmed">
+                    {quantities.middle}x
+                  </p>
+                  <p className="text-description text-white">Seat - Middle</p>
+                </div>
+                <div className="flex gap-x-1">
+                  <p className="text-description text-white">
+                    ${quantities.middle * pricesCategory.middle}
+                  </p>
+                  <p className="text-description text-white-dimmed">/ each</p>
+                </div>
               </div>
-              <div className="flex gap-x-1">
-                <p className="text-description text-white">$14.95</p>
-                <p className="text-description text-white-dimmed">/ each</p>
-              </div>
-            </div>
+            )}
 
-            <div className="flex justify-between py-2">
-              <div className="flex  gap-x-6">
-                <p className="text-description text-white-dimmed">2x</p>
-                <p className="text-description text-white">Seat - Back</p>
+            {quantities.back > 0 && (
+              <div className="flex justify-between py-2">
+                <div className="flex  gap-x-6">
+                  <p className="text-description text-white-dimmed">
+                    {quantities.back}x
+                  </p>
+                  <p className="text-description text-white">Seat - Back</p>
+                </div>
+                <div className="flex gap-x-1">
+                  <p className="text-description text-white">
+                    ${quantities.back * pricesCategory.back}
+                  </p>
+                  <p className="text-description text-white-dimmed">/ each</p>
+                </div>
               </div>
-              <div className="flex gap-x-1">
-                <p className="text-description text-white">$16.95</p>
-                <p className="text-description text-white-dimmed">/ each</p>
-              </div>
-            </div>
+            )}
           </div>
 
           <div className="pt-2">
@@ -69,7 +111,11 @@ export function CheckoutPanel({ seatsChosen }: CheckoutPanelProps) {
               {/* wrapper containing the total */}
               <div className="flex-col space-y-[3px]">
                 <p className="text-description">Total Price</p>
-                <p className="font-bold text-[20px] text-white">$43.55</p>
+                <p className="font-bold text-[20px] text-white">
+                  {quantities.front * pricesCategory.front +
+                    quantities.middle * pricesCategory.middle +
+                    quantities.back * pricesCategory.back}
+                </p>
               </div>
               <Button
                 variant="primary"
--- src/stores/BookingStore.ts
+++ src/stores/BookingStore.ts
@@ -1,21 +1,21 @@
-import create from "zustand"
-import { persist } from 'zustand/middleware'
+import create from "zustand";
+import { persist } from "zustand/middleware";
 
 type SeatStore = {
-  seatsChosen: string[]
-  setSeatsChosen: (seats: string[]) => void
-  clear: () => void
-}
+  seatsChosen: string[];
+  setSeatsChosen: (seats: string[]) => void;
+  clear: () => void;
+};
 
 export const useSeatStore = create<SeatStore>()(
-
-  persist((set) => ({
-
-    seatsChosen: [],
-    setSeatsChosen: (seats) => set({ seatsChosen: seats }),
-    clear: () => {
-      set({ seatsChosen: [] })
-    }
-
-  }), { name: 'seat-store' })
-)
+  persist(
+    (set) => ({
+      seatsChosen: [],
+      setSeatsChosen: (seats) => set({ seatsChosen: seats }),
+      clear: () => {
+        set({ seatsChosen: [] });
+      },
+    }),
+    { name: "seat-store" }
+  )
+);
