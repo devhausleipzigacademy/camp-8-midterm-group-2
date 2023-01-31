933179de17087bfe9d5c61317c6ce5f4774b38ed
Thomas <sappeiduda@gmail.com>
2023-01-31 15:12:36 +0100

    cleaning up models

--- server/models.ts
+++ server/models.ts
@@ -1,50 +1,51 @@
 import { z } from "zod";
+import { string } from "zod";
 
 const showingQueryModel = z.object({
-  dateTime: z.optional(string().refine((val) => {
-    try {
-      new Date.parse(val);
-    } catch (err) {
-      return false;
-    }
-    return true;
-  })),
+  dateTime: z.optional(
+    string().refine((val) => {
+      try {
+        Date.parse(val);
+      } catch (err) {
+        return false;
+      }
+      return true;
+    })
+  ),
 });
 
 const showingParamModel = z.object({
   movieId: z.string(),
 });
 
-const noReturnData = z.undefined()
+const noReturnData = z.undefined();
 
 const patchShowingParamsModel = z.object({
-    movieId: z.string(
-        datetime: z.string
-    )
-})
+  movieId: z.string(),
+  datetime: z.string(),
+});
 
-const patchUserBodyModel = z.object({movieId: z.string()})
+const patchUserBodyModel = z.object({ movieId: z.string() });
 
 const asyncPostUserBodyModel = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(8),
-    saltAndHash: z.string()
-})
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
+  saltAndHash: z.string(),
+});
 
 const postTokenBodyModel = z.object({
-    email: z.string().email(),
-    password: z.string().min(8)
+  email: z.string().email(),
+  password: z.string().min(8),
+});
 
-})
-
-const updateShowingModel = z.array
+const updateShowingModel = z.array;
 
 export default {
-    showingParamModel,
-    noReturnData,
-    asyncPostUserBodyModel,
-    patchShowingParamsModel,
-    patchUserBodyModel,
-    updateShowingModel
+  showingParamModel,
+  noReturnData,
+  asyncPostUserBodyModel,
+  patchShowingParamsModel,
+  patchUserBodyModel,
+  updateShowingModel,
 };
